e2dd66682e04ff22e7c3e7d6244ffc3388dea485
marinofaggiana <marino.faggiana@nextcloud.com>
2022-08-18 15:26:37 +0200

    first beta
    Signed-off-by: marinofaggiana <marino.faggiana@nextcloud.com>

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NextcloudKit",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
+    products: [
+        .library(
+            name: "NextcloudKit",
+            targets: ["NextcloudKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "5.4.1")),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/yahoojapan/SwiftyXMLParser", .upToNextMajor(from: "5.3.0")),
+    ],
+    targets: [
+        .target(
+            name: "NextcloudKit",
+            dependencies: ["Alamofire","SwiftyJSON","SwiftyXMLParser"],
+            path: "NextcloudKit"),
+        .testTarget(
+            name: "NextcloudKitTests",
+            dependencies: ["NextcloudKit"],
+            path: "NextcloudKitTests")
+    ]
+)
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# NextcloudKit
+# NextcloudKit [beta]
+
+## Installation
+
+### Carthage
+
+[Carthage](https://github.com/Carthage/Carthage) is a decentralized dependency manager that builds your dependencies and provides you with binary frameworks.
+
+To integrate **NextcloudKit** into your Xcode project using Carthage, specify it in your `Cartfile`:
+
+```
+github "nextcloud/NextcloudKit" "main"
+```
+
+Run `carthage update` to build the framework and drag the built `NextcloudKit.framework` into your Xcode project.
+
+### Manual
+
+To add ** NextcloudKit** to your app without Carthage, clone this repo and place it somewhere in your project folder. 
+Then, add `NextcloudKit.xcodeproj` to your project, select your app target and add the NextcloudKit framework as an embedded binary under `General` and as a target dependency under `Build Phases`.
