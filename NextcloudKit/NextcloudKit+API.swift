ef41d5419cfb0f349c28824b3015995dd3b64002
Marino Faggiana <marino@marinofaggiana.com>
2022-09-27 18:03:36 +0200

    wrapper async/await downloadPreview
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- NextcloudKit/NextcloudKit+API.swift
+++ NextcloudKit/NextcloudKit+API.swift
@@ -279,7 +279,7 @@ extension NextcloudKit {
     }
 
     @available(iOS 13.0, *)
-    public func downloadAsyncPreview(fileNamePathOrFileId: String,
+    public func downloadPreview(fileNamePathOrFileId: String,
                                    fileNamePreviewLocalPath: String,
                                    widthPreview: Int,
                                    heightPreview: Int,
