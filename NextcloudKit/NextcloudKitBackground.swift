222c81dde870d1b38d08d4047076ad9461ef1b8d
Marino Faggiana <marino@marinofaggiana.com>
2022-10-27 10:39:10 +0200

    add log auth
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- NextcloudKit/NextcloudKitBackground.swift
+++ NextcloudKit/NextcloudKitBackground.swift
@@ -235,6 +235,9 @@ import Foundation
             completionHandler(URLSession.AuthChallengeDisposition.performDefaultHandling, nil)
         } else {
             NKCommon.shared.delegate?.authenticationChallenge?(session, didReceive: challenge, completionHandler: { authChallengeDisposition, credential in
+                if NKCommon.shared.levelLog > 1 {
+                    NKCommon.shared.writeLog("[INFO AUTH] authChallengeDisposition \(authChallengeDisposition)")
+                }
                 completionHandler(authChallengeDisposition, credential)
             })
         }
