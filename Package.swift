9a9affd5140bc77c1c740d719fd9b2c0e5bee91c
Marino Faggiana <marino@marinofaggiana.com>
2023-02-24 15:56:21 +0100

    Apply swiftlint
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Package.swift
+++ Package.swift
@@ -14,12 +14,12 @@ let package = Package(
     products: [
         .library(
             name: "NextcloudKit",
-            targets: ["NextcloudKit"])
+            targets: ["NextcloudKit"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "5.4.1")),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/yahoojapan/SwiftyXMLParser", .upToNextMajor(from: "5.3.0"))
+        .package(url: "https://github.com/yahoojapan/SwiftyXMLParser", .upToNextMajor(from: "5.3.0")),
     ],
     targets: [
         .target(
