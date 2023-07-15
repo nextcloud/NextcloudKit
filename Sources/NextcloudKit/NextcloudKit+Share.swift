531fba0bd8c89f504759bdc5fc8d778647238a23
Marino Faggiana <marino@marinofaggiana.com>
2023-07-15 19:04:28 +0200

    Update NextcloudKit+Share.swift

--- Sources/NextcloudKit/NextcloudKit+Share.swift
+++ Sources/NextcloudKit/NextcloudKit+Share.swift
@@ -383,7 +383,11 @@ extension NextcloudKit {
         }
         parameters["publicUpload"] = publicUpload == true ? "true" : "false"
         parameters["hideDownload"] = hideDownload == true ? "true" : "false"
-        parameters["attributes"] = attributes
+        if let attributes = attributes {
+            parameters["attributes"] = attributes
+        } else {
+            parameters["attributes"] = "null"
+        }
 
         sessionManager.request(url, method: .put, parameters: parameters, encoding: URLEncoding.default, headers: headers, interceptor: nil).validate(statusCode: 200..<300).responseData(queue: self.nkCommonInstance.backgroundQueue) { response in
             debugPrint(response)
