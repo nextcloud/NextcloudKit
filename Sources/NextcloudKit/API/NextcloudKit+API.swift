707e2e074a90c4d3f93501361dabc045c6cbd83a
Marino Faggiana <marino@marinofaggiana.com>
2023-10-12 12:53:29 +0200

    sendClientDiagnosticRemoteOperation
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Sources/NextcloudKit/API/NextcloudKit+API.swift
+++ Sources/NextcloudKit/API/NextcloudKit+API.swift
@@ -868,4 +868,46 @@ extension NextcloudKit {
             }
         }
     }
+
+    // MARK: -
+
+    // {"problems":{"conflict":{"count":3,"oldest":1695592800},"failed-upload":{"count":1,"oldest":1695592900}}}
+
+    public func sendClientDiagnosticRemoteOperation(problems: String,
+                                                    options: NKRequestOptions = NKRequestOptions(),
+                                                    completion: @escaping (_ account: String, _ error: NKError) -> Void) {
+
+        let account = self.nkCommonInstance.account
+        let urlBase = self.nkCommonInstance.urlBase
+
+        let endpoint = "ocs/v2.php/apps/settings/diagnostic"
+
+        guard let url = self.nkCommonInstance.createStandardUrl(serverUrl: urlBase, endpoint: endpoint) else {
+            return options.queue.async { completion(account, .urlError) }
+        }
+
+        let headers = self.nkCommonInstance.getStandardHeaders(options.customHeader, customUserAgent: options.customUserAgent, contentType: "application/json")
+
+        var urlRequest: URLRequest
+        do {
+            try urlRequest = URLRequest(url: url, method: .put, headers: headers)
+            urlRequest.httpBody = problems.data(using: .utf8)
+        } catch {
+            return options.queue.async { completion(account, NKError(error: error)) }
+        }
+
+        sessionManager.request(urlRequest).validate(statusCode: 200..<300).response(queue: self.nkCommonInstance.backgroundQueue) { response in
+            if self.nkCommonInstance.levelLog > 0 {
+                debugPrint(response)
+            }
+
+            switch response.result {
+            case .failure(let error):
+                let error = NKError(error: error, afResponse: response, responseData: response.data)
+                options.queue.async { completion(account, error) }
+            case .success:
+                options.queue.async { completion(account, .success) }
+            }
+        }
+    }
 }
