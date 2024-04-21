6e1a7067dabd216ed9f994b7dd0cf8dfa8c40344
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-04-21 17:33:40 +0800

    Add visionOS target support to NextcloudKit
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,8 @@ let package = Package(
         .macOS(.v10_15),
         .iOS(.v10),
         .tvOS(.v10),
-        .watchOS(.v3)
+        .watchOS(.v3),
+        .visionOS(.v1)
     ],
     products: [
         .library(
