170308602449e26456dbb450707fcd567b281eb1
Milen Pivchev <milen.pivchev@gmail.com>
2024-11-06 14:14:05 +0100

    WIP
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

--- Tests/NextcloudKitIntegrationTests/FilesIntegrationTests.swift
+++ Tests/NextcloudKitIntegrationTests/FilesIntegrationTests.swift
@@ -6,52 +6,52 @@ import XCTest
 @testable import NextcloudKit
 
 final class FilesIntegrationTests: BaseIntegrationXCTestCase {
-//    func test_createReadDeleteFolder_withProperParams_shouldCreateReadDeleteFolder() throws {
-//        let expectation = expectation(description: "Should finish last callback")
-//        let folderName = "TestFolder\(randomInt)"
-//        let serverUrl = "\(TestConstants.server)/remote.php/dav/files/\(TestConstants.username)"
-//        let serverUrlFileName = "\(serverUrl)/\(folderName)"
-//
-//        NextcloudKit.shared.appendSession(account: TestConstants.account, urlBase: TestConstants.server, user: TestConstants.username, userId: TestConstants.username, password: TestConstants.password, userAgent: "", nextcloudVersion: 0, groupIdentifier: "")
-//
-////        // Test creating folder
-////        NextcloudKit.shared.createFolder(serverUrlFileName: serverUrlFileName, account: account) { account, ocId, date, error in
-////            XCTAssertEqual(self.account, account)
-////
-////            XCTAssertEqual(NKError.success.errorCode, error.errorCode)
-////            XCTAssertEqual(NKError.success.errorDescription, error.errorDescription)
-////
-////            Thread.sleep(forTimeInterval: 0.2)
-////
-////            // Test reading folder, should exist
-////            NextcloudKit.shared.readFileOrFolder(serverUrlFileName: serverUrlFileName, depth: "0", account: account) { account, files, data, error in
-////                XCTAssertEqual(self.account, account)
-////                XCTAssertEqual(NKError.success.errorCode, error.errorCode)
-////                XCTAssertEqual(NKError.success.errorDescription, error.errorDescription)
-////                XCTAssertEqual(files?[0].fileName, folderName)
-////
-////                Thread.sleep(forTimeInterval: 0.2)
-////
-////                // Test deleting folder
-////                NextcloudKit.shared.deleteFileOrFolder(serverUrlFileName: serverUrlFileName, account: account) { account, error in
-////                    XCTAssertEqual(self.account, account)
-////                    XCTAssertEqual(NKError.success.errorCode, error.errorCode)
-////                    XCTAssertEqual(NKError.success.errorDescription, error.errorDescription)
-////
-////                    Thread.sleep(forTimeInterval: 0.2)
-////
-////                    // Test reading folder, should NOT exist
-////                    NextcloudKit.shared.readFileOrFolder(serverUrlFileName: serverUrlFileName, depth: "0", account: account) { account, files, data, error in
-////                        defer { expectation.fulfill() }
-////
-////                        XCTAssertEqual(404, error.errorCode)
-////                        XCTAssertEqual(self.account, account)
-////                        XCTAssertTrue(files?.isEmpty ?? false)
-////                    }
-////                }
-////            }
-////        }
-//
-//        waitForExpectations(timeout: 100)
-//    }
+    func test_createReadDeleteFolder_withProperParams_shouldCreateReadDeleteFolder() throws {
+        let expectation = expectation(description: "Should finish last callback")
+        let folderName = "TestFolder\(randomInt)"
+        let serverUrl = "\(TestConstants.server)/remote.php/dav/files/\(TestConstants.username)"
+        let serverUrlFileName = "\(serverUrl)/\(folderName)"
+
+        NextcloudKit.shared.appendSession(account: TestConstants.account, urlBase: TestConstants.server, user: TestConstants.username, userId: TestConstants.username, password: TestConstants.password, userAgent: "", nextcloudVersion: 0, groupIdentifier: "")
+
+        // Test creating folder
+        NextcloudKit.shared.createFolder(serverUrlFileName: serverUrlFileName, account: TestConstants.account) { account, ocId, date, _, error in
+            XCTAssertEqual(TestConstants.account, account)
+
+            XCTAssertEqual(NKError.success.errorCode, error.errorCode)
+            XCTAssertEqual(NKError.success.errorDescription, error.errorDescription)
+
+            Thread.sleep(forTimeInterval: 0.2)
+
+            // Test reading folder, should exist
+            NextcloudKit.shared.readFileOrFolder(serverUrlFileName: serverUrlFileName, depth: "0", account: account) { account, files, data, error in
+                XCTAssertEqual(TestConstants.account, account)
+                XCTAssertEqual(NKError.success.errorCode, error.errorCode)
+                XCTAssertEqual(NKError.success.errorDescription, error.errorDescription)
+                XCTAssertEqual(files?[0].fileName, folderName)
+
+                Thread.sleep(forTimeInterval: 0.2)
+
+                // Test deleting folder
+                NextcloudKit.shared.deleteFileOrFolder(serverUrlFileName: serverUrlFileName, account: account) { account, _, error in
+                    XCTAssertEqual(TestConstants.account, account)
+                    XCTAssertEqual(NKError.success.errorCode, error.errorCode)
+                    XCTAssertEqual(NKError.success.errorDescription, error.errorDescription)
+
+                    Thread.sleep(forTimeInterval: 0.2)
+
+                    // Test reading folder, should NOT exist
+                    NextcloudKit.shared.readFileOrFolder(serverUrlFileName: serverUrlFileName, depth: "0", account: account) { account, files, data, error in
+                        defer { expectation.fulfill() }
+
+                        XCTAssertEqual(404, error.errorCode)
+                        XCTAssertEqual(TestConstants.account, account)
+                        XCTAssertTrue(files?.isEmpty ?? false)
+                    }
+                }
+            }
+        }
+
+        waitForExpectations(timeout: 100)
+    }
 }
--- Tests/NextcloudKitIntegrationTests/ShareIntegrationTests.swift
+++ Tests/NextcloudKitIntegrationTests/ShareIntegrationTests.swift
@@ -16,25 +16,25 @@ final class ShareIntegrationTests: BaseIntegrationXCTestCase {
 
         NextcloudKit.shared.appendSession(account: TestConstants.account, urlBase: TestConstants.server, user: TestConstants.username, userId: TestConstants.username, password: TestConstants.password, userAgent: "", nextcloudVersion: 0, groupIdentifier: "")
 
-//        NextcloudKit.shared.createFolder(serverUrlFileName: serverUrlFileName, account: account) { account, ocId, date, error in
-//            XCTAssertEqual(self.account, account)
-//
-//            XCTAssertEqual(NKError.success.errorCode, error.errorCode)
-//            XCTAssertEqual(NKError.success.errorDescription, error.errorDescription)
-//            
-//            Thread.sleep(forTimeInterval: 0.2)
-//
-//            let note = "Test note"
-//
-//            NextcloudKit.shared.createShare(path: folderName, shareType: 0, shareWith: "nextcloud", note: note, account: "") { account, share, data, error in
-//                defer { expectation.fulfill() }
-//
-//                XCTAssertEqual(self.account, account)
-//                XCTAssertEqual(NKError.success.errorCode, error.errorCode)
-//                XCTAssertEqual(NKError.success.errorDescription, error.errorDescription)
-//                XCTAssertEqual(note, share?.note)
-//            }
-//        }
+        NextcloudKit.shared.createFolder(serverUrlFileName: serverUrlFileName, account: TestConstants.account) { account, ocId, date, _, error in
+            XCTAssertEqual(TestConstants.account, account)
+
+            XCTAssertEqual(NKError.success.errorCode, error.errorCode)
+            XCTAssertEqual(NKError.success.errorDescription, error.errorDescription)
+            
+            Thread.sleep(forTimeInterval: 0.2)
+
+            let note = "Test note"
+
+            NextcloudKit.shared.createShare(path: folderName, shareType: 0, shareWith: "nextcloud", note: note, account: "") { account, share, data, error in
+                defer { expectation.fulfill() }
+
+                XCTAssertEqual(TestConstants.account, account)
+                XCTAssertEqual(NKError.success.errorCode, error.errorCode)
+                XCTAssertEqual(NKError.success.errorDescription, error.errorDescription)
+                XCTAssertEqual(note, share?.note)
+            }
+        }
 
         waitForExpectations(timeout: 100)
     }
