1084c4a0b464ac40b18355dc4269486a159d73a2
Marino Faggiana <marino@marinofaggiana.com>
2024-12-10 10:19:13 +0100

    getTermsOfService
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Sources/NextcloudKit/NextcloudKit+ToS.swift
+++ Sources/NextcloudKit/NextcloudKit+ToS.swift
@@ -7,5 +7,40 @@ import Alamofire
 import SwiftyJSON
 
 public extension NextcloudKit {
-    
+    func getTermsOfService(account: String,
+                           options: NKRequestOptions = NKRequestOptions(),
+                           request: @escaping (DataRequest?) -> Void = { _ in },
+                           taskHandler: @escaping (_ task: URLSessionTask) -> Void = { _ in },
+                           completion: @escaping (_ account: String, _ error: NKError) -> Void) {
+        let endpoint = "ocs/v2.php/apps/terms_of_service/terms"
+        guard let nkSession = nkCommonInstance.getSession(account: account),
+              let url = nkCommonInstance.createStandardUrl(serverUrl: nkSession.urlBase, endpoint: endpoint, options: options),
+              let headers = nkCommonInstance.getStandardHeaders(account: account, options: options) else {
+            return options.queue.async { completion(account, .urlError) }
+        }
+
+        let tosRequest = nkSession.sessionData.request(url, method: .get, encoding: URLEncoding.default, headers: headers, interceptor: nil).validate(statusCode: 200..<300).onURLSessionTaskCreation { task in
+            task.taskDescription = options.taskDescription
+            taskHandler(task)
+        }.responseData(queue: self.nkCommonInstance.backgroundQueue) { response in
+            if self.nkCommonInstance.levelLog > 0 {
+                debugPrint(response)
+            }
+            switch response.result {
+            case .success(let jsonData):
+                let json = JSON(jsonData)
+                let data = json["ocs"]["data"]
+                let statusCode = json["ocs"]["meta"]["statuscode"].int ?? NKError.internalError
+                if 200..<300 ~= statusCode {
+                    options.queue.async { completion(account, .success) }
+                } else {
+                    options.queue.async { completion(account,NKError(rootJson: json, fallbackStatusCode: response.response?.statusCode)) }
+                }
+            case .failure(let error):
+                let error = NKError(error: error, afResponse: response, responseData: response.data)
+                options.queue.async { completion(account, error) }
+            }
+        }
+        options.queue.async { request(tosRequest) }
+    }
 }
