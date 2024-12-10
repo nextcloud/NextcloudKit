e3ca649e9bf60317218eebda04245766f3a385cc
Marino Faggiana <marino@marinofaggiana.com>
2024-12-10 10:06:22 +0100

    tos
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

---
+++ Sources/NextcloudKit/NextcloudKit+ToS.swift
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: Nextcloud GmbH
+// SPDX-FileCopyrightText: 2024 Marino Faggiana
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+public extension NextcloudKit {
+    
+}
