b5c71ed32a96ea6c3210b7c42fa86aca8a77d68b
Marino Faggiana <marino@marinofaggiana.com>
2025-02-02 18:08:16 +0100

    cod
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Sources/NextcloudKit/NKMonitor.swift
+++ Sources/NextcloudKit/NKMonitor.swift
@@ -37,13 +37,16 @@ final class NKMonitor: EventMonitor, Sendable {
               let headerValue = request.request?.allHTTPHeaderFields?["X-NC-CheckUnauthorized"],
               headerValue.lowercased() == "true",
               let account = request.request?.allHTTPHeaderFields?["X-NC-Account"] as? String {
+               self.readFile(serverUrlFileName: "", account: account) { account, error in
+                   /*
+                   var unauthorizedArray = groupDefaults?.array(forKey: "Unauthorized") as? [String] ?? []
+                   if !unauthorizedArray.contains(account) {
+                       unauthorizedArray.append(account)
+                       groupDefaults?.set(unauthorizedArray, forKey: "Unauthorized")
 
-               var unauthorizedArray = groupDefaults?.array(forKey: "Unauthorized") as? [String] ?? []
-               if !unauthorizedArray.contains(account) {
-                   unauthorizedArray.append(account)
-                   groupDefaults?.set(unauthorizedArray, forKey: "Unauthorized")
-
-                   self.nkCommonInstance.writeLog("Unauthorized set for account: \(account)")
+                       self.nkCommonInstance.writeLog("Unauthorized set for account: \(account)")
+                   }
+                   */
                }
            } else if statusCode == 503 {
                print("503 Service Unavailable")
@@ -69,6 +72,49 @@ final class NKMonitor: EventMonitor, Sendable {
             } else {
                 self.nkCommonInstance.writeLog("Network response result: \(date) " + responseDebugDescription)
                 self.nkCommonInstance.writeLog("Network response all headers: \(date) " + responseAllHeaderFields)
+            }
+        }
+    }
+
+    func readFile(serverUrlFileName: String,
+                  account: String,
+                  options: NKRequestOptions = NKRequestOptions(),
+                  taskHandler: @escaping (_ task: URLSessionTask) -> Void = { _ in },
+                  completion: @escaping (_ account: String, _ error: NKError) -> Void) {
+        ///
+        options.contentType = "application/xml"
+        ///
+        guard let nkSession = nkCommonInstance.getSession(account: account),
+              let url = serverUrlFileName.encodedToUrl,
+              var headers = nkCommonInstance.getStandardHeaders(account: account, checkUnauthorized: true, options: options) else {
+            return options.queue.async { completion(account, .urlError) }
+        }
+
+        let method = HTTPMethod(rawValue: "PROPFIND")
+        headers.update(name: "Depth", value: "0")
+        var urlRequest: URLRequest
+
+        do {
+            try urlRequest = URLRequest(url: url, method: method, headers: headers)
+            urlRequest.httpBody = NKDataFileXML(nkCommonInstance: self.nkCommonInstance).getRequestBodyFile(createProperties: options.createProperties, removeProperties: options.removeProperties).data(using: .utf8)
+
+        } catch {
+            return options.queue.async { completion(account, NKError(error: error)) }
+        }
+
+        nkSession.sessionData.request(urlRequest, interceptor: NKInterceptor()).validate(statusCode: 200..<300).onURLSessionTaskCreation { task in
+            task.taskDescription = options.taskDescription
+            taskHandler(task)
+        }.responseData(queue: self.nkCommonInstance.backgroundQueue) { response in
+            if self.nkCommonInstance.levelLog > 0 {
+                debugPrint(response)
+            }
+            switch response.result {
+            case .failure(let error):
+                let error = NKError(error: error, afResponse: response, responseData: response.data)
+                options.queue.async { completion(account, error) }
+            case .success:
+                options.queue.async { completion(account, .success) }
             }
         }
     }
