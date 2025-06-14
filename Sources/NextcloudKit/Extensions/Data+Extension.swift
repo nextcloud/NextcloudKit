e1181a839b81c9c657687ef7ff16127c0cdb4dbe
Marino Faggiana <marino.faggiana@nextcloud.com>
2025-06-14 08:52:12 +0200

    Update Sources/NextcloudKit/Extensions/Data+Extension.swift
    Co-authored-by: Marcel Müller <marcel-mueller@gmx.de>
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

--- Sources/NextcloudKit/Extensions/Data+Extension.swift
+++ Sources/NextcloudKit/Extensions/Data+Extension.swift
@@ -10,7 +10,7 @@ extension Data {
             let json = try JSONSerialization.jsonObject(with: self, options: [])
             let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
             guard let jsonString = String(data: data, encoding: .utf8) else {
-                print("Inavlid data")
+                print("Invalid data")
                 return
             }
             print(jsonString)
