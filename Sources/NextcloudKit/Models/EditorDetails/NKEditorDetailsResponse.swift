09c33cd088ded42732d695b85e866dc822373432
Marino Faggiana <marino@marinofaggiana.com>
2025-06-19 09:58:17 +0200

    fix parser
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Sources/NextcloudKit/Models/EditorDetails/NKEditorDetailsResponse.swift
+++ Sources/NextcloudKit/Models/EditorDetails/NKEditorDetailsResponse.swift
@@ -1,13 +1,13 @@
 
 import Foundation
 
-public struct NKEditorDetailsResponse: Codable {
+public struct NKEditorDetailsResponse: Codable, Sendable {
     public let ocs: OCS
 
-    public struct OCS: Codable {
+    public struct OCS: Codable, Sendable {
         public let data: DataClass
 
-        public struct DataClass: Codable {
+        public struct DataClass: Codable, Sendable {
             public let editors: [String: NKEditorDetailsEditor]
             public let creators: [String: NKEditorDetailsCreator]
         }
@@ -27,19 +27,20 @@ public struct NKEditorTemplateResponse: Codable, Sendable {
 }
 
 public struct NKEditorDetailsEditor: Codable, Sendable {
-    public let name: String
+    public let id: String
     public let mimetypes: [String]
-    public let optionalMimetypes: [String]?
-    public let secure: Int
+    public let name: String
+    public let optionalMimetypes: [String]
+    public let secure: Bool
 }
 
 public struct NKEditorDetailsCreator: Codable, Sendable {
-    public let editor: String
-    public let `extension`: String
     public let id: String
+    public let templates: Bool
     public let mimetype: String
     public let name: String
-    public let templates: Int
+    public let editor: String
+    public let `extension`: String
 }
 
 public struct NKEditorTemplate: Codable, Sendable {
