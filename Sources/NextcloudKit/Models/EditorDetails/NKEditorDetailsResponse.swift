3a9e89c98b59017bf5d196f6bc966a95a37340d6
Marino Faggiana <marino@marinofaggiana.com>
2025-06-19 10:26:55 +0200

    code
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Sources/NextcloudKit/Models/EditorDetails/NKEditorDetailsResponse.swift
+++ Sources/NextcloudKit/Models/EditorDetails/NKEditorDetailsResponse.swift
@@ -27,25 +27,49 @@ public struct NKEditorTemplateResponse: Codable, Sendable {
 }
 
 public struct NKEditorDetailsEditor: Codable, Sendable {
-    public let id: String
+    public let identifier: String
     public let mimetypes: [String]
     public let name: String
     public let optionalMimetypes: [String]
     public let secure: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case identifier = "id"
+        case mimetypes
+        case name
+        case optionalMimetypes
+        case secure
+    }
 }
 
 public struct NKEditorDetailsCreator: Codable, Sendable {
-    public let id: String
+    public let identifier: String
     public let templates: Bool
     public let mimetype: String
     public let name: String
     public let editor: String
-    public let `extension`: String
+    public let ext: String
+
+    enum CodingKeys: String, CodingKey {
+        case identifier = "id"
+        case templates
+        case mimetype
+        case name
+        case editor
+        case ext = "extension"
+    }
 }
 
 public struct NKEditorTemplate: Codable, Sendable {
-    public let `extension`: String
-    public let id: String
+    public let ext: String
+    public let identifier: String
     public let name: String
     public let preview: String
+
+    enum CodingKeys: String, CodingKey {
+        case ext = "extension"
+        case identifier = "id"
+        case name
+        case preview
+    }
 }
