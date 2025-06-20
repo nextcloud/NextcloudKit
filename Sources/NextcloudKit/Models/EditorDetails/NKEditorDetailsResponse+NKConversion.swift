b318a327146e136ceee9d2448aceab7cfa451e2f
Marino Faggiana <marino@marinofaggiana.com>
2025-06-20 12:07:04 +0200

    license
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Sources/NextcloudKit/Models/EditorDetails/NKEditorDetailsResponse+NKConversion.swift
+++ Sources/NextcloudKit/Models/EditorDetails/NKEditorDetailsResponse+NKConversion.swift
@@ -1,8 +1,10 @@
+// SPDX-FileCopyrightText: Nextcloud GmbH
+// SPDX-FileCopyrightText: 2025 Marino Faggiana
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 import Foundation
 
-
 public extension NKEditorDetailsResponse.OCS.DataClass {
-
     func editorsArray() -> [NKEditorDetailsEditor] {
         Array(editors.values)
     }
