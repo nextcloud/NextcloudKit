789cfc518d265f7dca576ad048b356e59ed908fd
Marino Faggiana <marino@marinofaggiana.com>
2025-06-24 16:41:34 +0200

    fix
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Sources/NextcloudKit/NextcloudKit+E2EE.swift
+++ Sources/NextcloudKit/NextcloudKit+E2EE.swift
@@ -168,7 +168,7 @@ public extension NextcloudKit {
                               e2eToken: String? = nil,
                               account: String,
                               options: NKRequestOptions = NKRequestOptions(),
-                              taskHandler: @escaping (_ task: URLSessionTask) -> Void = { _ in }) async -> (account: String, e2eToken: String?, responseData: AFDataResponse<Data>?, error: NKError)  {
+                              taskHandler: @escaping (_ task: URLSessionTask) -> Void = { _ in }) async -> (account: String, e2eMetadata: String?, signature: String?, responseData: AFDataResponse<Data>?, error: NKError)  {
         await withCheckedContinuation { continuation in
             getE2EEMetadata(
                 fileId: fileId,
@@ -177,7 +177,7 @@ public extension NextcloudKit {
                 options: options,
                 taskHandler: taskHandler
             ) { account, e2eMetadata, signature, responseData, error in
-                continuation.resume(returning: (account: account, e2eToken: e2eToken, responseData: responseData, error: error))
+                continuation.resume(returning: (account: account, e2eMetadata: e2eMetadata, signature: signature, responseData: responseData, error: error))
             }
         }
     }
