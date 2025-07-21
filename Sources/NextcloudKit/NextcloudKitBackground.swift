d4730114ceab1853a48302c2dc7caba6fc16f139
Marino Faggiana <marino@marinofaggiana.com>
2025-07-21 14:30:55 +0200

    cod
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Sources/NextcloudKit/NextcloudKitBackground.swift
+++ Sources/NextcloudKit/NextcloudKitBackground.swift
@@ -53,7 +53,7 @@ public final class NKBackground: NSObject, URLSessionTaskDelegate, URLSessionDel
             url = (serverUrlFileName as? String)?.encodedToUrl as? URL
         }
 
-        guard let nkSession = nkCommonInstance.nksessions.session(forAccount: account),
+        guard var nkSession = nkCommonInstance.nksessions.session(forAccount: account),
               let urlForRequest = url
         else {
             return (nil, .urlError)
@@ -161,7 +161,7 @@ public final class NKBackground: NSObject, URLSessionTaskDelegate, URLSessionDel
             url = (serverUrlFileName as? String)?.encodedToUrl as? URL
         }
 
-        guard let nkSession = nkCommonInstance.nksessions.session(forAccount: account),
+        guard var nkSession = nkCommonInstance.nksessions.session(forAccount: account),
               let urlForRequest = url
         else {
             return (nil, .urlError)
