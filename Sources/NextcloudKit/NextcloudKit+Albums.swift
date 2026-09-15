57eef7258084ac5b74ccad941206af4be7fcc510
Marino Faggiana <marino.faggiana@nextcloud.com>
2026-09-15 15:37:13 +0200

    create sourcecode
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

---
+++ Sources/NextcloudKit/NextcloudKit+Albums.swift
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: Nextcloud GmbH
+// SPDX-FileCopyrightText: 2026 Dhanesh
+// SPDX-FileCopyrightText: 2026 Marino Faggiana
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import Foundation
+import Alamofire
+
+public extension NextcloudKit {
+}
