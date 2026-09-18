7f68f18060c2ee6d9d25357737336ed72b23327f
Marino Faggiana <marino.faggiana@nextcloud.com>
2026-09-18 11:20:10 +0200

    fix
    Signed-off-by: Marino Faggiana <marino.faggiana@nextcloud.com>

--- Sources/NextcloudKit/NextcloudKit+WebDAV.swift
+++ Sources/NextcloudKit/NextcloudKit+WebDAV.swift
@@ -206,7 +206,8 @@ public extension NextcloudKit {
         nkSession.sessionData.request(urlRequest, interceptor: NKInterceptor(nkCommonInstance: nkCommonInstance)).validate(statusCode: 200..<300).onURLSessionTaskCreation { task in
             task.taskDescription = options.taskDescription
             taskHandler(task)
-        }.responseData(queue: self.nkCommonInstance.backgroundQueue) { response in
+        }.response(queue: self.nkCommonInstance.backgroundQueue) { response in
+            let response = response.map { $0 ?? Data() }
             let result = self.evaluateResponse(response)
 
             options.queue.async {
